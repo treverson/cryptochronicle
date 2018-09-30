3f95596ec465c2d608714225c57bda1f34070a82
ookimaki <ookimaki@givinglog.com>
2018-09-30 17:25:56 +0900

    add easy_loom

---
+++ games/flappyVoenista/connection.js
@@ -0,0 +1,53 @@
+var contract;
+var from;
+
+//Replace with your contract ABI
+var contractABI = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "add",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+//Replace with your contract address
+var contractAddress = "0x1a31b9b9d281d49001fe7f3f638000a739afc9c3";
+
+window.addEventListener(`load`, async function () {
+        const privateKey = loom.CryptoUtils.generatePrivateKey()
+		const publicKey = loom.CryptoUtils.publicKeyFromPrivateKey(privateKey)
+		
+        const client = new loom.Client(
+            'default',
+            //This is public loom sandbox for cryptochronicles			
+            'ws://160.16.137.244:46657/websocket',
+            'ws://160.16.137.244:9999/queryws'
+        )
+
+		from = loom.LocalAddress.fromPublicKey(publicKey).toString()
+		
+		console.log("Your wallet address:" + from);
+		const web3 = new Web3(new loom.LoomProvider(client, privateKey))
+		
+
+		contract = new web3.eth.Contract(contractABI, contractAddress, { from })
+		
+});
---
+++ library/easy_loom_sdk/README.md
@@ -0,0 +1,4 @@
+# Load below to use in html
+
+        <script src="https://cdn.jsdelivr.net/npm/loom-js@1.15.0/dist/loom.umd.js"></script>
+        <script type="text/javascript" src="connection.js"></script>
