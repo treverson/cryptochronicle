630e4bd59570ac92c16e968af672aa8fded7600b
ookimaki <38954280+ookimaki@users.noreply.github.com>
2018-10-20 18:16:00 +0900

    Update connection.js

--- games/flappyVoenista/connection.js
+++ games/flappyVoenista/connection.js
@@ -38,8 +38,8 @@ window.addEventListener(`load`, async function () {
         const client = new loom.Client(
             'default',
             //This is public loom sandbox for cryptochronicles			
-            'wss://160.16.137.244:46658/websocket',
-	    'wss://160.16.137.244:46658/queryws'
+            'ws://160.16.137.244:46658/websocket',
+	    'ws://160.16.137.244:46658/queryws'
         )
 
 		from = loom.LocalAddress.fromPublicKey(publicKey).toString()
