3be772b585ed4adc4447560fd9d2b52a77a16c6f
ookimaki <38954280+ookimaki@users.noreply.github.com>
2018-10-20 20:02:36 +0900

    Update connection.js

--- games/flappyVoenista/connection.js
+++ games/flappyVoenista/connection.js
@@ -32,22 +32,21 @@ var contractABI = [
 var contractAddress = "0x1a31b9b9d281d49001fe7f3f638000a739afc9c3";
 
 window.addEventListener(`load`, async function () {
-        const privateKey = loom.CryptoUtils.generatePrivateKey()
-		const publicKey = loom.CryptoUtils.publicKeyFromPrivateKey(privateKey)
-		
         const client = new loom.Client(
             'default',
-            //This is public loom sandbox for cryptochronicles			
             'ws://160.16.137.244:46658/websocket',
-	    'ws://160.16.137.244:46658/queryws'
+			'ws://160.16.137.244:46658/queryws'
+			//'ws://127.0.0.1:46658/websocket',
+			//'ws://127.0.0.1:46658/queryws'
         )
 
-		from = loom.LocalAddress.fromPublicKey(publicKey).toString()
-		
-		console.log("Your wallet address:" + from);
-		const web3 = new Web3(new loom.LoomProvider(client, privateKey))
-		
+        const privateKey = loom.CryptoUtils.generatePrivateKey()
+	const publicKey = loom.CryptoUtils.publicKeyFromPrivateKey(privateKey)		
 
-		contract = new web3.eth.Contract(contractABI, contractAddress, {from: from})
+	from = loom.LocalAddress.fromPublicKey(publicKey).toString()
+
+	console.log("Your wallet address:" + from);
+	const web3 = new Web3(new loom.LoomProvider(client, privateKey))
+	contract = new web3.eth.Contract(contractABI, contractAddress, {from: from})
 		
 });
